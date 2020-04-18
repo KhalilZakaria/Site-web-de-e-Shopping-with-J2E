a6d0dfa2928c5df1a66f7c65db984af86ec2f422
Khalil Zakaria <zakaria.khalil1999@gmail.com>
2020-04-18 14:22:31 +0100

    Update Insertion.java

--- src/Insertion.java
+++ src/Insertion.java
@@ -2,7 +2,7 @@ import java.sql.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.*;
-//pour insérer un utilisateur
+//pour insÃ©rer un utilisateur
 public class Insertion extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse res)throws
 	ServletException, IOException {
@@ -13,13 +13,13 @@ public class Insertion extends HttpServlet {
 			String fprenom=req.getParameter("prenom");
 			String femail=req.getParameter("email");
 			String fpassword=req.getParameter("mdp");
-			//connexion à la bd
+			//connexion Ã  la bd
 			String url="jdbc:mysql://localhost:3306/mini_projet?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 			String driver = "com.mysql.jdbc.Driver";
 			Class.forName(driver).newInstance();
 			Connection con;
-			con=DriverManager.getConnection(url,"root","Zakaria@1999");
-			//insérer dans la taille de personnes
+			con=DriverManager.getConnection(url,"root","Zak1998");
+			//insÃ©rer dans la taille de personnes
 			PreparedStatement stmt=con.prepareStatement("insert into personnes(nom,prenom,email,password,isAdmin) values (?,?,?,?,?)");
 			stmt.setString(1,fnom);
 			stmt.setString(2,fprenom);
